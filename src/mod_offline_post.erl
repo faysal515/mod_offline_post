3b8eec93f79fbeb3c92bbf170a04fb86efc94488
Jan <jan.mudzki@gmail.com>
2017-08-15 18:23:10 +0800

    improve parsing

--- src/mod_offline_post.erl
+++ src/mod_offline_post.erl
@@ -30,21 +30,30 @@ stop(Host) ->
 			      ?MODULE, send_notice, 10),
 	ok.
 
-send_notice({Action,Packet}) ->	
-	From = element(5,Packet),
-	To = element(6,Packet),
+send_notice({Action,Packet}) ->
+	From = Packet#message.from,
+	To = Packet#message.to,
 	
-	From1 = From#jid.luser,
-	To1 = To#jid.luser,
+	FromUsername = binary_to_list(From#jid.luser),
+	ToUsername = binary_to_list(To#jid.luser),
 	
-	Body = binary_to_list(element(3,lists:nth(1,element(8,Packet)))),
+	Body = binary_to_list(xmpp:get_text(Packet#message.body)),
+	
+	%% Convert back to xmlel
+	El = xmpp:encode(Packet),
+	
+	%% Custom data/subelement of your own from the message
+	CustomData = fxml:get_path_s(El, [{elem, list_to_binary("data")}]),
+	IsPhoto = binary_to_list(fxml:get_tag_attr_s(list_to_binary("isPhoto"), CustomData)),
 	
 	PostUrl = gen_mod:get_module_opt(To#jid.lserver, ?MODULE, post_url,fun(S) -> iolist_to_binary(S) end, list_to_binary("")),
+	
+	%% Configure your own options passed to module
 	AppId = gen_mod:get_module_opt(To#jid.lserver, ?MODULE, app_id, fun(S) -> iolist_to_binary(S) end, list_to_binary("")),
 	ApiKey = gen_mod:get_module_opt(To#jid.lserver, ?MODULE, api_key, fun(S) -> iolist_to_binary(S) end, list_to_binary("")),
 	
-	Data = string:join(["to=", binary_to_list(To1), "&from=", binary_to_list(From1), "&body=", Body], ""),
+	Data = string:join(["to=", ToUsername, "&from=", FromUsername, "&body=", Body, "&isPhoto=", IsPhoto], ""),
 	Request = {binary_to_list(PostUrl), [{"X-Parse-Application-Id", binary_to_list(AppId)}, {"X-Parse-REST-API-Key", binary_to_list(ApiKey)}], "application/x-www-form-urlencoded", Data},
 	httpc:request(post, Request,[],[]),
-			
+
 	ok.
